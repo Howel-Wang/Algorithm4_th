f2d93f0ea1128de4c4cbf8864288860a3ba597a0
Howel-Wang <351844926@qq.com>
2018-11-20 18:49:21 +0800

    update

--- src/Unit1/Practice1_16.java
+++ src/Unit1/Practice1_16.java
@@ -3,7 +3,7 @@ package Unit1;
 import java.util.Scanner;
 
 /*
- 	给出exR1(6)的返回值：
+ 	1.1.16给出exR1(6)的返回值：
  	public static String exR1(int n){
  		if(n <= 0) return "";
  		return exR1(n-3) + n + exR1(n-2) + n;
