55224a2cdbd53a0042a401456821d2501c34a654
Howel-Wang <351844926@qq.com>
2019-05-20 00:22:29 +0800

    practice1_1_10.java

--- src/Unit1/Practice1_1_10.java
+++ src/Unit1/Practice1_1_10.java
@@ -1,10 +1,10 @@
 package Unit1;
 /*
-	1.1.10
-	下面这段代码有什么问题？
+?	1.1.10
+?	下面这段代码有什么问题？
 
- 	答：会出现Exception in thread "main" java.lang.Error: Unresolved compilation problem:
-	The local variable a may not have been initialized
+?	答：会出现Exception in thread "main" java.lang.Error: Unresolved compilation problem:
+?	The local variable a may not have been initialized
 */
 
 
@@ -13,7 +13,7 @@ public class Practice1_1_10 {
 	public static void show(String[] args) {
 		int[] a;
 		for(int i = 0;i<10;i++) {
-//			a[i] = i*i;
+			// a[i] = i*i;
 		}
 		
 	}
