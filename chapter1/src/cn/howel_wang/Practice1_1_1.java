c675c3830e2267f26ce31dc5ab944eb6a75dbe02
Howel-Wang <351844926@qq.com>
2020-03-26 21:29:31 +0800

    modify

---
+++ chapter1/src/cn/howel_wang/Practice1_1_1.java
@@ -0,0 +1,14 @@
+package cn.howel_wang;
+/**
+ * 1.1.1 给出以下表达式的值：
+        a. ( 0 + 15 ) / 2
+        b. 2.0e-6 * 100000000.1
+        c. true && false || true && true
+ */
+public class Practice1_1_1{
+    public static void main(String[] args) {
+        System.out.println((0 + 15) / 2);
+        System.out.println(2.0*e - 6 * 100000000.1);
+        System.out.println(true && false || true && false);
+    }
+}
