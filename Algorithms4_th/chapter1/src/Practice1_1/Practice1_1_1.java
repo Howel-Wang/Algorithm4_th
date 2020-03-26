3cb26cc064f27fddbc4f1eafb64066df74a54bad
Howel-Wang <351844926@qq.com>
2020-03-26 22:10:19 +0800

    add

---
+++ Algorithms4_th/chapter1/src/Practice1_1/Practice1_1_1.java
@@ -0,0 +1,27 @@
+package Practice1_1;
+/**
+ * 1.1.1 给出以下表达式的值：
+ *
+ * a. ( 0 + 15 ) / 2
+ *
+ * b. 2.0e-6 * 100000000.1
+ *
+ * c. true && false || true && true
+ */
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * @author howel
+ * @version 1.0.0
+ * @ClassName Practice1_1_1
+ * @Description
+ * @createTime 2020年03月26日 20:42:05
+ */
+public class Practice1_1_1 {
+    public static void main(String[] args) {
+        StdOut.println(( 0 + 15 ) / 2);
+        StdOut.println(2.0e-6 * 100000000.1);
+        StdOut.println(true && false || true && true);
+    }
+}
