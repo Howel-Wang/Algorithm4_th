836ad93f96d934470c0455438394e346ae7ef2b1
Howel-Wang <351844926@qq.com>
2020-03-26 14:54:08 +0800

    modify

--- chapter_1/src/app/App.java
+++ chapter_1/src/app/App.java
@@ -1,7 +1,7 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
         System.out.println("Hello Java");
     }
 }
