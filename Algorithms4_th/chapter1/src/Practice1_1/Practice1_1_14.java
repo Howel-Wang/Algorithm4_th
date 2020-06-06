53a97d519a2db9ac71cc67d71434c143c4ccb18f
Howel-Wang <351844926@qq.com>
2020-06-06 14:43:35 +0800

    update 1_1_9

--- Algorithms4_th/chapter1/src/Practice1_1/Practice1_1_14.java
+++ Algorithms4_th/chapter1/src/Practice1_1/Practice1_1_14.java
@@ -2,6 +2,8 @@ package Practice1_1;
 
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Scanner;
+
 /**
  * Q: 编写一个静态方法 lg() ，接受一个整型参数 N ，返回不大于 log 2N 的最大整数。不要使用 Math 库。
  *
@@ -11,15 +13,16 @@ public class Practice1_1_14 {
     public static int lg(int N) {
         int x = 0;
         int sum = 1;
-        do {
+        while (sum <= 2 * N) {
             sum = sum * 10;
             x++;
-        } while (sum < 2 * N);
+        }
+        x--;
         return x;
     }
 
     public static void main(String[] args) {
-        int N = 400;
+        int N = new Scanner(System.in).nextInt();
         int result = lg(N);
         StdOut.println(result);
     }
