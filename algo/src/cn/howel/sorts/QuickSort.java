8a9c25180a867f654f69859dbedf69d6ac9b9418
wanghailin <351844926@qq.com>
2020-10-16 14:11:55 +0800

    delete quickSort&selectionSort

---
+++ algo/src/cn/howel/sorts/QuickSort.java
@@ -0,0 +1,31 @@
+package cn.howel.sorts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author howel
+ * @version 1.0.0
+ * @Description
+ */
+public class QuickSort {
+
+    public static int[] quickSort(int[] a,int n) {
+        if (a.length < 2) {
+            return a;
+        }else {
+            int temp = a[0];
+            for (int i = 1; i < a.length; i++) {
+                if (a[i] < temp) {
+
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ algo/src/cn/howel/sorts/SelectionSort.java
@@ -0,0 +1,41 @@
+package cn.howel.sorts;
+
+
+/**
+ * @author howel
+ * @version 1.0.0
+ * @Description 选择排序
+ */
+public class SelectionSort {
+
+    public static void selectionSort(int[] a, int n) {
+
+        if (n < 2) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int tmp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3,4,2,1,5,6,7,8};
+        for (int i : array) {
+            System.out.printf("%d\t", i);
+        }
+        System.out.println();
+        selectionSort(array, array.length);
+        for (int i : array) {
+            System.out.printf("%d\t", i);
+        }
+    }
+
+}
