84ae5cf8b5c9e18780b67e233cff7e41eb859fa8
wanghailin <351844926@qq.com>
2021-04-13 22:31:32 +0800

    add some files

---
+++ LeetCode/src/cn/howel/goldenInterview/practice01_02.java
@@ -0,0 +1,34 @@
+package cn.howel.goldenInterview;
+
+/**
+ * @author howel
+ * @version 1.0.0
+ * @Description
+ *     给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+ *
+ *     示例 1：
+ *
+ *     输入: s1 = "abc", s2 = "bca"
+ *     输出: true
+ *     示例 2：
+ *
+ *     输入: s1 = "abc", s2 = "bad"
+ *     输出: false
+ *     说明：
+ *
+ *     0 <= len(s1) <= 100
+ *     0 <= len(s2) <= 100
+ */
+public class practice01_02 {
+
+    public static boolean CheckPermutation(String s1, String s2) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "bca";
+
+    }
+}
--- algorithm4/src/main/java/cn/howel/chapter1/section1/Practice1.java
+++ algorithm4/src/main/java/cn/howel/chapter1/section1/Practice1.java
@@ -13,8 +13,9 @@ import edu.princeton.cs.algs4.StdOut;
  * @createTime 2020年03月26日 20:42:05
  */
 public class Practice1 {
+
     public static void main(String[] args) {
-        StdOut.println(( 0 + 15 ) / 2);
+        StdOut.println((0 + 15) / 2);
         StdOut.println(2.0e-6 * 100000000.1);
         StdOut.println(true && false || true && true);
     }
