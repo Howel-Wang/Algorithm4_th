cf21441e2b4a94b46e30f229f4389e88adeae676
howel-wang <351844926@qq.com>
2021-04-14 15:30:47 +0800

    âupdate practice01_01

--- LeetCode/src/cn/howel/goldenInterview/practice01_01.java
+++ LeetCode/src/cn/howel/goldenInterview/practice01_01.java
@@ -1,3 +1,19 @@
+/**
+    实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+
+    示例 1：
+
+    输入: s = "leetcode"
+    输出: false
+    示例 2：
+
+    输入: s = "abc"
+    输出: true
+    限制：
+
+    0 <= len(s) <= 100
+    如果你不使用额外的数据结构，会很加分。
+*/
 package cn.howel.goldenInterview;
 
 /**
@@ -8,7 +24,7 @@ package cn.howel.goldenInterview;
 public class practice01_01 {
 
     public static boolean isUnique(String astr) {
-        String s = "";
+        /*String s = "";
         if (astr.length() == 1) return true;
         if (astr.length() == 2) {
             if (astr.substring(0,1).equals(astr.substring(1,2))) {
@@ -23,13 +39,37 @@ public class practice01_01 {
                 }
             }
         }
+        return true;*/
+        long low64 = 0;
+        long high64 = 0;
+
+        for (char c : astr.toCharArray()) {
+            if (c >= 64) {
+                long bitIndex = 1L << (c - 64);
+                if ((high64 & bitIndex) != 0) {
+                    return false;
+                }
+                high64 |= bitIndex;
+            } else {
+                long bitIndex = 1L << c;
+                if ((low64 & bitIndex) != 0) {
+                    return false;
+                }
+                low64 |= bitIndex;
+            }
+        }
         return true;
     }
 
     public static void main(String[] args) {
-        String a = "leetcode";
-        String b = "abc";
-        System.out.println(isUnique(a));
-        System.out.println(isUnique(b));
+        String a = "abcdefghijklmnopqrstuvwxyzz";
+        boolean result = false;
+        long l1 = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            result = isUnique(a);
+        }
+        long l2 = System.currentTimeMillis();
+        System.out.println(result);
+        System.out.println(l2-l1);
     }
 }
