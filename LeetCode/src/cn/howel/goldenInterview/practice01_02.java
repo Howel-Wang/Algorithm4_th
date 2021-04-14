d81dccd1b22e488813abab6e169350c80a14bf58
wanghailin <351844926@qq.com>
2021-04-15 00:38:31 +0800

    update practice01_02.java

--- LeetCode/src/cn/howel/goldenInterview/practice01_02.java
+++ LeetCode/src/cn/howel/goldenInterview/practice01_02.java
@@ -1,12 +1,12 @@
 package cn.howel.goldenInterview;
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * @author howel
  * @version 1.0.0
- * @Description 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。 示例 1：
+ * @Description 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+ * 示例 1：
  * <p>
  * 输入: s1 = "abc", s2 = "bca" 输出: true 示例 2：
  * <p>
