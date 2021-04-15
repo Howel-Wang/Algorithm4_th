b82be8f3848f6ed33465a545f40c5da9d531e940
wanghailin <351844926@qq.com>
2021-04-15 19:58:46 +0800

    update Solution1.java

--- LeetCode/src/cn/howel/middle/Solution1.java
+++ LeetCode/src/cn/howel/middle/Solution1.java
@@ -51,7 +51,6 @@ public class Solution1 {
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-        ListNode node2 = new ListNode(5, new ListNode(6, new ListNode(4)));
         System.out.println(getValSum(node1));
 
     }
