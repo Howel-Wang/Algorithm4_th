051d3b0c94581778600a99a46cb507286485f995
howel-wang <351844926@qq.com>
2021-04-16 12:44:27 +0800

    update practice01_02

--- LeetCode/src/cn/howel/goldenInterview/practice01_02.java
+++ LeetCode/src/cn/howel/goldenInterview/practice01_02.java
@@ -5,12 +5,16 @@ import java.util.Arrays;
 /**
  * @author howel
  * @version 1.0.0
- * @Description 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
- * 示例 1：
- * <p>
- * 输入: s1 = "abc", s2 = "bca" 输出: true 示例 2：
- * <p>
- * 输入: s1 = "abc", s2 = "bad" 输出: false 说明： 0 <= len(s1) <= 100 0 <= len(s2) <=100
+ * @Description
+ *
+ * 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+ *      示例 1：
+ *
+ *      输入: s1 = "abc", s2 = "bca" 输出: true 示例 2：
+ *
+ *      输入: s1 = "abc", s2 = "bad" 输出: false
+ *      说明： 0 <= len(s1) <= 100
+ *            0 <= len(s2) <= 100
  */
 public class practice01_02 {
 
